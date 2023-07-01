7cfe8a6be787ca9f7eab492b4123a071eb053771
James Mitsumasu <jrmitsumasu@gmail.com>
2023-06-30 22:48:08 -0400

    Initial

---
+++ src/main/java/com/coderscampus/ShelfApp/ShelfAppApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.ShelfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfAppApplication.class, args);
+	}
+
+}
